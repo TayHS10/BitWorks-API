fe19d342e5df05f5e47a8b6608fe05eabb05a01c
D1ec18ch8 <jeffersonmoragaloria@gmail.com>
2025-06-02 19:59:42 -0600

    creación de funciones en UserControllerUser
    Se agregaron las funciones:
    -list: para ver la cantidad de usuarios
    -create: para crear usuarios
    -searchEmail: para buscar por email al usuario
    -searchID: para buscar id al usuario
    ---------
    nota aparte: se le agrego la propiedad password al userDTO

--- GPP_API/DTO/User/UserDTO.cs
+++ GPP_API/DTO/User/UserDTO.cs
@@ -5,6 +5,7 @@
         public int UserId { get; set; }
         public string FullName { get; set; } = null!;
         public string Email { get; set; } = null!;
+        public string Password { get; set; }
         public string Role { get; set; } = null!;
     }
 
